3b67a58dd8bbd1c82f216ca75b2fc5f7260c313e
mevlutcantuna <mttuna90@gmail.com>
2022-08-10 22:16:49 +0300

    firebase updated

--- client/src/firebase/index.ts
+++ client/src/firebase/index.ts
@@ -2,7 +2,7 @@
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
+// updated
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
