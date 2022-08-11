72fb59a821e1140b5777fef02432684208447331
mevlutcantuna <mttuna90@gmail.com>
2022-08-11 16:14:34 +0300

    router structure and private route added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import PrivateRoute from "./components/PrivateRoute";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <PrivateRoute>
+                <Home />
+              </PrivateRoute>
+            }
+          />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
-}
+};
 
 export default App;
---
+++ client/src/components/PrivateRoute.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Navigate } from 'react-router-dom'
+
+type Props = {
+    children: JSX.Element,
+}
+
+const PrivateRoute: React.FC<Props> = ({ children }) => {
+    const token = localStorage.getItem('token')
+
+    if (!token) {
+        return <Navigate to="/login" replace />
+    }
+    return children;
+}
+
+export default PrivateRoute
